6a0c5f2c98e0406a668cb4ffe31dc1cf454181e3
utso <cccygodev@gmail.com>
2020-10-06 21:27:03 +0200

    Initial commit (including Killua)

---
+++ cards-hxh1/script/c20000355.lua
@@ -0,0 +1,71 @@
+--Hunter X Transmuter - Killua
+local s,id=GetID()
+function s.initial_effect(c)
+    --Special Summon
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetRange(LOCATION_HAND)
+    e1:SetType(EFFECT_TYPE_QUICK_O)
+    e1:SetCode(EVENT_CHAINING)
+    e1:SetCondition(s.condition1)
+    e1:SetTarget(s.target1)
+    e1:SetOperation(s.operation1)
+    e1:SetCountLimit(1,id)
+    c:RegisterEffect(e1)
+    --Negate
+    local e2=Effect.CreateEffect(c)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetCode(EFFECT_DISABLE)
+    e2:SetTargetRange(0,LOCATION_MZONE)
+    e2:SetTarget(s.target2)
+    c:RegisterEffect(e2)
+    --Destroy
+    local e3=Effect.CreateEffect(c)
+    e3:SetCategory(CATEGORY_DESTROY)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(EVENT_BATTLE_CONFIRM)
+    e3:SetCondition(s.condition3)
+    e3:SetTarget(s.target3)
+    e3:SetOperation(s.operation3)
+    c:RegisterEffect(e3)
+end
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
+  return rp==1-tp and re:IsActiveType(TYPE_MONSTER)
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then 
+    return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
+  end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+  local c=e:GetHandler()
+  if c:IsRelateToEffect(e) then
+    Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+  end
+end
+function s.target2(e,c)
+  return c==e:GetHandler():GetBattleTarget()
+end
+function s.filter3(c)
+  return c:IsFaceup() and c:IsSetCard(0x200)
+end
+function s.condition3(e,tp,eg,ep,ev,re,r,rp)
+  return Duel.IsExistingMatchingCard(s.filter3,tp,LOCATION_MZONE,0,1,e:GetHandler())
+end
+function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
+	local t=Duel.GetAttackTarget()
+	if chk==0 then return Duel.GetAttacker()==e:GetHandler() and t~=nil end
+	Duel.SetTargetCard(t)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,t,1,0,0)
+end
+function s.operation3(e,tp,eg,ep,ev,re,r,rp)
+	local t=Duel.GetFirstTarget()
+	if t:IsRelateToBattle() then
+		Duel.Destroy(t,REASON_EFFECT)
+	end
+end
