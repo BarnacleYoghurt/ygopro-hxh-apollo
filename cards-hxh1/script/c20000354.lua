a947e1929ca4bb5d83c73053b2c89073dcf71d30
utso <cccygodev@gmail.com>
2020-10-07 11:08:53 +0200

    Add card descriptions for Gon, Kurapika, Leorio

---
+++ cards-hxh1/script/c20000354.lua
@@ -0,0 +1,5 @@
+-- Hunter X Enhancer - Gon
+local s, id = GetID()
+function s.initial_effect(c)
+  -- effects
+end
---
+++ cards-hxh1/script/c20000356.lua
@@ -0,0 +1,5 @@
+-- Hunter X Conjurator - Kurapika
+local s, id = GetID()
+function s.initial_effect(c)
+  -- effects
+end
