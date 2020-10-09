281ea7767702e6eca65ad0850a9dfba6bc28cbba
utso <cccygodev@gmail.com>
2020-10-09 10:51:42 +0200

    Nen Training: Test and corrections

--- cards-hxh1/script/c20000358.lua
+++ cards-hxh1/script/c20000358.lua
@@ -10,38 +10,36 @@ function s.initial_effect(c)
   local e1=Effect.CreateEffect(c)
   e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
   e1:SetDescription(aux.Stringid(id,0))
-  e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+  e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+  e1:SetRange(LOCATION_SZONE)
   e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
   e1:SetCode(EVENT_DESTROYED)
-  e1:SetCondition(s.condition1)
   e1:SetTarget(s.target1)
   e1:SetOperation(s.operation1)
   e1:SetCountLimit(1,id)
   c:RegisterEffect(e1)
 end
-function s.filter1a(c,e,tp)
-  return c:IsSetCard(0x200) and c:IsType(TYPE_MONSTER) 
-    and c:IsControler(tp) and c:IsReason(REASON_BATTLE+REASON_EFFECT) 
-    and c:GetReasonPlayer()==1-tp 
-    and Duel.IsExistingMatchingCard(s.filter1b,tp,LOCATION_DECK,0,1,nil,e,tp,c:GetOriginalLevel())
+function s.filter1a(c,e,tp,rp)
+  return c:IsPreviousSetCard(0x200) and c:IsType(TYPE_MONSTER)
+    and (c:IsReason(REASON_BATTLE) or (rp==1-tp and c:IsReason(REASON_EFFECT)))
+    and c:IsPreviousLocation(LOCATION_MZONE) and c:GetPreviousControler()==tp
+    and c:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and c:IsCanBeEffectTarget(e)
+    and c:GetOriginalLevel()>0 and Duel.IsExistingMatchingCard(s.filter1b,tp,LOCATION_DECK,0,1,nil,e,tp,c:GetOriginalLevel())
 end
 function s.filter1b(c,e,tp,lvl)
-  return c:IsSetCard(0x200) and c:IsLevelBelow(lvl) 
-    and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.condition1(e,tp,eg,ep,ev,re,r,rp)
-  return eg:IsExists(s.filter1a,1,nil,tp)
+  return c:IsSetCard(0x200) and c:GetLevel()<lvl and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-  local pg=eg:Filter(s.filter1a,nil,e,tp):Filter(Card.IsCanBeEffectTarget,nil,e)
+  local pg=eg:Filter(s.filter1a,nil,e,tp,rp)
   if chkc then return pg:IsContains(chkc) end
   if chk==0 then return pg:GetCount()>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
   local tg=pg:Select(pg,tp,1,1,nil)
   Duel.SetTargetCard(tg)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-  if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+  if not e:GetHandler():IsRelateToEffect(e) or Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
   local tc=Duel.GetFirstTarget()
   if tc:IsRelateToEffect(e) then
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
