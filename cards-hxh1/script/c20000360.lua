b5c4631c4584a04dd5c1cd223745d8780587cd55
utso <cccygodev@gmail.com>
2020-10-12 15:51:27 +0200

    Implement Emperor Chains

--- cards-hxh1/script/c20000360.lua
+++ cards-hxh1/script/c20000360.lua
@@ -1,9 +1,71 @@
--- Emperor Chains
-local s, id = GetID()
+--Emperor Chains
+local s,id=GetID()
 function s.initial_effect(c)
-  -- activate
-  local e1 = Effect.CreateEffect(c)
+  --Activate
+  local e1=Effect.CreateEffect(c)
+  e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_CONTINUOUS_TARGET)
   e1:SetType(EFFECT_TYPE_ACTIVATE)
   e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER)
+  e1:SetTarget(s.target1)
+  e1:SetOperation(s.operation1)
   c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_CHAIN_SOLVED)
+	e2:SetLabelObject(e1)
+	e2:SetCondition(s.condition2)
+	e2:SetOperation(s.operation2)
+	c:RegisterEffect(e2)
+  --Summon restriction
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetTargetRange(1,1)
+	e3:SetTarget(s.target3)
+	c:RegisterEffect(e3)
+	--Destroy
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetCode(EVENT_LEAVE_FIELD)
+	e4:SetCondition(s.condition4)
+	e4:SetOperation(s.operation4)
+	c:RegisterEffect(e4)
+end
+function s.filter1(c)
+  return c:IsFaceup() and c:IsCode(20000356)
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+  if chkc then return chkc:IsCanBeEffectTarget(e) and chkc:IsLocation(LOCATION_MZONE) and s.filter1(chkc) end
+  if chk==0 then return Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+  local g=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+end
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+	return re==e:GetLabelObject()
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS):GetFirst()
+	if c:IsRelateToEffect(re) and tc:IsFaceup() and tc:IsRelateToEffect(re) then
+		c:SetCardTarget(tc)
+	end
+end
+function s.target3(e,c,tp,sumtp,sumpos)
+	local tc=e:GetHandler():GetFirstCardTarget()
+	return tc and c:IsAttackBelow(tc:GetBaseAttack())
+end
+function s.condition4(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsStatus(STATUS_DESTROY_CONFIRMED) then return false end
+	local tc=e:GetHandler():GetFirstCardTarget()
+	return tc and eg:IsContains(tc)
+end
+function s.operation4(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
 end
