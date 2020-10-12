471e14d87a5a759920beef04c04c79d33f6b0d54
utso <cccygodev@gmail.com>
2020-10-12 12:53:57 +0200

    Implement Kaminari

--- cards-hxh1/script/c20000359.lua
+++ cards-hxh1/script/c20000359.lua
@@ -1,9 +1,37 @@
--- Kaminari
-local s, id = GetID()
+--Kaminari
+local s,id=GetID()
 function s.initial_effect(c)
-  -- activate
+  --Activate
   local e1 = Effect.CreateEffect(c)
+  e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
   e1:SetType(EFFECT_TYPE_ACTIVATE)
   e1:SetCode(EVENT_FREE_CHAIN)
+  e1:SetTarget(s.target1)
+  e1:SetOperation(s.operation1)
   c:RegisterEffect(e1)
+end
+function s.filter1(c)
+  return c:IsFaceup() and c:IsSetCard(0x200)
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+  if chkc then return chkc:IsCanBeEffectTarget(e) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter1(chkc) end
+  if chk==0 then return Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil) end
+  local g=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+  local tc=Duel.GetFirstTarget()
+  if tc:IsRelateToEffect(e) then
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+    e1:SetValue(1)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+    tc:RegisterEffect(e1)
+    local e2=Effect.CreateEffect(e:GetHandler())
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_IMMUNE_EFFECT)
+    e2:SetValue(1)
+    e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+    tc:RegisterEffect(e2)
+  end
 end
