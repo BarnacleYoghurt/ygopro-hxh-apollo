7b5ee6e227dead9d606d1f329b974f02d8d9840a
utso <cccygodev@gmail.com>
2020-10-13 18:15:30 +0200

    Generate script templates for new cards

---
+++ cards-hxh1/script/c20000312.lua
@@ -0,0 +1,5 @@
+-- Hunter X Manipulator - Shoot
+local s, id = GetID()
+function s.initial_effect(c)
+  -- effects
+end
---
+++ cards-hxh1/script/c20000315.lua
@@ -0,0 +1,5 @@
+-- Hunter X Enhancer - Biscuit
+local s, id = GetID()
+function s.initial_effect(c)
+  -- effects
+end
