7e52da201968f85796ec357ae65d63b4a31edc0d
utso <cccygodev@gmail.com>
2020-10-13 10:28:08 +0200

    Implement Departure

--- cards-hxh1/script/c20000377.lua
+++ cards-hxh1/script/c20000377.lua
@@ -1,9 +1,28 @@
--- Departure
-local s, id = GetID()
+--Departure
+local s,id=GetID()
 function s.initial_effect(c)
-  -- activate
+  --Activate
   local e1 = Effect.CreateEffect(c)
+  e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
   e1:SetType(EFFECT_TYPE_ACTIVATE)
   e1:SetCode(EVENT_FREE_CHAIN)
+  e1:SetTarget(s.target1)
+  e1:SetOperation(s.operation1)
+  e1:SetCountLimit(1,id)
   c:RegisterEffect(e1)
+end
+function s.filter1(c)
+	return c:IsSetCard(0x200) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end
