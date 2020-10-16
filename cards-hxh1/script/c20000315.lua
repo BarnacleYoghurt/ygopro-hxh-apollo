bb1e498b230f3e329a8553482155ebc13e2eb031
utso <cccygodev@gmail.com>
2020-10-16 12:08:51 +0200

    Implement Biscuit

--- cards-hxh1/script/c20000315.lua
+++ cards-hxh1/script/c20000315.lua
@@ -1,5 +1,54 @@
--- Hunter X Enhancer - Biscuit
-local s, id = GetID()
+--Hunter X Enhancer - Biscuit
+local s,id=GetID()
 function s.initial_effect(c)
-  -- effects
+  --Special Summon
+  local e1=Effect.CreateEffect(c)
+  e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+  e1:SetDescription(aux.Stringid(id,0))
+  e1:SetRange(LOCATION_HAND)
+  e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+  e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+  e1:SetCondition(s.condition1)
+  e1:SetTarget(s.target1)
+  e1:SetOperation(s.operation1)
+  e1:SetCountLimit(1,id)
+  c:RegisterEffect(e1)
+  --Change positions
+  local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_POSITION)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+  e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
+	c:RegisterEffect(e2)
+end
+function s.filter1(c,sp)
+  return c:GetSummonPlayer()==sp and c:IsPreviousLocation(LOCATION_EXTRA)
+end
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
+  return eg:IsExists(s.filter1,1,nil,1-tp)
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then 
+    return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
+  end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+  local c=e:GetHandler()
+  if c:IsRelateToEffect(e) then
+    Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+  end
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0 end
+	local sg=Duel.GetFieldGroup(tp,0,LOCATION_MZONE)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,sg,sg:GetCount(),0,0)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+	local sg=Duel.GetFieldGroup(tp,0,LOCATION_MZONE)
+	if sg:GetCount()>0 then
+		Duel.ChangePosition(sg,POS_FACEUP_DEFENSE,POS_FACEDOWN_DEFENSE,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK,true)
+	end
 end
