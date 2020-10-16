b3cc11653c432b2926a5da45336e64d555744ebb
utso <cccygodev@gmail.com>
2020-10-16 12:49:21 +0200

    Implement Knuckle

--- cards-hxh1/script/c20000319.lua
+++ cards-hxh1/script/c20000319.lua
@@ -1,5 +1,65 @@
--- Hunter X Emitter - Knuckle
-local s, id = GetID()
+--Hunter X Emitter - Knuckle
+local s,id=GetID()
 function s.initial_effect(c)
-  -- effects
+  --Special Summon
+  local e1=Effect.CreateEffect(c)
+  e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+  e1:SetDescription(aux.Stringid(id,0))
+  e1:SetRange(LOCATION_HAND)
+  e1:SetType(EFFECT_TYPE_IGNITION)
+  e1:SetCondition(s.condition1)
+  e1:SetTarget(s.target1)
+  e1:SetOperation(s.operation1)
+  e1:SetCountLimit(1,id)
+  c:RegisterEffect(e1)
+  --Place counter
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_COUNTER)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+  e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
+	e2:SetCountLimit(1)
+	c:RegisterEffect(e2)
+  --Negate
+  local e3=Effect.CreateEffect(c)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_DISABLE)
+	e3:SetTargetRange(0,LOCATION_MZONE)
+	e3:SetTarget(s.target3)
+	c:RegisterEffect(e3)
+end
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
+  return Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)==1
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then 
+    return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
+  end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+  local c=e:GetHandler()
+  if c:IsRelateToEffect(e) then
+    Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+  end
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsCanBeEffectTarget(e) and chkc:IsControler(1-tp) and chkc:IsCanAddCounter(0x1f01,1) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsCanAddCounter,tp,0,LOCATION_MZONE,1,nil,0x1f01,1) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,Card.IsCanAddCounter,tp,0,LOCATION_MZONE,1,1,nil,0x1f01,1)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		tc:AddCounter(0x1f01,1)
+	end
+end
+function s.target3(e,c)
+	return c:GetCounter(0x1f01)>0 and (c:IsType(TYPE_EFFECT) or bit.band(c:GetOriginalType(),TYPE_EFFECT)==TYPE_EFFECT)
 end
