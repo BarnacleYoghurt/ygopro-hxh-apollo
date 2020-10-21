db6b379c4b21114bf752feb6f5a6765f3ba1a33d
utso <cccygodev@gmail.com>
2020-10-21 11:20:55 +0200

    Implement Hyper Enhance

--- cards-hxh1/script/c20000318.lua
+++ cards-hxh1/script/c20000318.lua
@@ -1,9 +1,49 @@
--- Hyper Enhance
-local s, id = GetID()
+--Hyper Enhance
+local s,id=GetID()
 function s.initial_effect(c)
-  -- activate
+  --Activate
   local e1 = Effect.CreateEffect(c)
+  e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+  e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
   e1:SetType(EFFECT_TYPE_ACTIVATE)
   e1:SetCode(EVENT_FREE_CHAIN)
+  e1:SetHintTiming(TIMING_DAMAGE_STEP)
+  e1:SetCondition(s.condition1)
+  e1:SetTarget(s.target1)
+  e1:SetOperation(s.operation1)
   c:RegisterEffect(e1)
+end
+function s.filter1(c)
+  return c:IsFaceup() and c:IsSetCard(0xf01)
+end
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+  if chkc then return chkc:IsCanBeEffectTarget(e) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter1(chkc) end
+  if chk==0 then return Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil) end
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+  local g=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetValue(tc:GetAttack()*2)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_SET_DEFENSE_FINAL)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e2:SetValue(tc:GetDefense()*2)
+		tc:RegisterEffect(e2)
+		local e3=Effect.CreateEffect(e:GetHandler())
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
+		e3:SetValue(aux.ChangeBattleDamage(1,HALF_DAMAGE))
+		tc:RegisterEffect(e3)
+	end
 end
