f01153f4e3296eb57dcbeadce4bd0428877e79ac
utso <cccygodev@gmail.com>
2020-10-22 12:01:31 +0200

    Implement Bomber Ambush

--- cards-hxh1/script/c20000363.lua
+++ cards-hxh1/script/c20000363.lua
@@ -1,9 +1,72 @@
--- Bomber Ambush
-local s, id = GetID()
+--Bomber Ambush
+local s,id=GetID()
 function s.initial_effect(c)
-  -- activate
-  local e1 = Effect.CreateEffect(c)
+  --Activate
+  local e1=Effect.CreateEffect(c)
+  e1:SetCategory(CATEGORY_DESTROY)
+  e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
   e1:SetType(EFFECT_TYPE_ACTIVATE)
-  e1:SetCode(EVENT_FREE_CHAIN)
+  e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+  e1:SetCondition(s.condition1)
+  e1:SetTarget(s.target1)
+  e1:SetOperation(s.operation1)
   c:RegisterEffect(e1)
+  --Destroy
+  local e2=Effect.CreateEffect(c)
+  e2:SetCategory(CATEGORY_DESTROY)
+  e2:SetDescription(aux.Stringid(id,0))
+  e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+  e2:SetCode(EVENT_DESTROYED)
+  e2:SetCondition(s.condition2)
+  e2:SetTarget(s.target2)
+  e2:SetOperation(s.operation2)
+  c:RegisterEffect(e2)
+end
+function s.filter1a(c)
+	return c:IsFaceup() and c:IsSetCard(0xf01)
+end
+function s.filter1b(c,atk)
+  return c:IsFaceup() and c:GetAttack()<atk
+end
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
+	return tp~=Duel.GetTurnPlayer() and Duel.IsExistingMatchingCard(s.filter1a,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local tc=Duel.GetAttacker()
+	if chkc then return chkc==tc end
+	if chk==0 then return tc:IsOnField() and tc:IsCanBeEffectTarget(e) and Duel.IsExistingMatchingCard(s.filter1b,tp,0,LOCATION_MZONE,1,nil,tc:GetAttack()) end
+	Duel.SetTargetCard(tc)
+  local g=Duel.GetMatchingGroup(s.filter1b,tp,0,LOCATION_MZONE,nil,tc:GetAttack())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:CanAttack() and not tc:IsStatus(STATUS_ATTACK_CANCELED) then
+    local g=Duel.GetMatchingGroup(s.filter1b,tp,0,LOCATION_MZONE,nil,tc:GetAttack())
+    if g:GetCount()>0 then
+      Duel.Destroy(g,REASON_EFFECT)
+    end
+	end
+end
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+  local c=e:GetHandler()
+	return c:IsReason(REASON_EFFECT) and rp==1-tp and c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsPreviousPosition(POS_FACEDOWN)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	local tg=g:GetMaxGroup(Card.GetAttack)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,tg,1,0,0)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	if g:GetCount()>0 then
+		local tg=g:GetMaxGroup(Card.GetAttack)
+		if tg:GetCount()>1 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+			local sg=tg:Select(tp,1,1,nil)
+			Duel.HintSelection(sg)
+			Duel.Destroy(sg,REASON_EFFECT)
+		else Duel.Destroy(tg,REASON_EFFECT) end
+	end
 end
