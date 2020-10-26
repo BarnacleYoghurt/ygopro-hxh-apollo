c0f08e990063174542530acfa0ef566c210783e1
utso <cccygodev@gmail.com>
2020-10-26 16:52:46 +0100

    Implement Crazy Slots

--- cards-hxh1/script/c20000387.lua
+++ cards-hxh1/script/c20000387.lua
@@ -1,9 +1,34 @@
--- Crazy Slots
-local s, id = GetID()
+--Crazy Slots
+local s,id=GetID()
 function s.initial_effect(c)
-  -- activate
+  --Activate
   local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DRAW+CATEGORY_HANDES+CATEGORY_DICE)
   e1:SetType(EFFECT_TYPE_ACTIVATE)
   e1:SetCode(EVENT_FREE_CHAIN)
+  e1:SetCondition(s.condition1)
+  e1:SetTarget(s.target1)
+  e1:SetOperation(s.operation1)
+  e1:SetCountLimit(1,id)
   c:RegisterEffect(e1)
+end
+s.roll_dice=true
+function s.filter1(c)
+  return c:IsSetCard(0xf01) and c:IsType(TYPE_MONSTER)
+end
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
+  return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_GRAVE,0,1,nil)
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_HAND,0) or Duel.IsPlayerCanDraw(tp,6-Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)) end
+	Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+	local dc=Duel.TossDice(1-tp,1)
+  local gc=Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)
+  if dc>gc then
+    Duel.Draw(tp,dc-gc,REASON_EFFECT)
+  else
+    Duel.DiscardHand(tp,aux.TRUE,gc-dc,gc-dc,REASON_EFFECT,nil)
+  end
 end
