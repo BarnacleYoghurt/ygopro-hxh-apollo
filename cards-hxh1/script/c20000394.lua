4cede1c59fe7b4bb67e8c3da144e07c6a4a09bbb
utso <cccygodev@gmail.com>
2020-11-02 18:12:18 +0100

    Implement Enraged Enhancer

--- cards-hxh1/script/c20000394.lua
+++ cards-hxh1/script/c20000394.lua
@@ -1,5 +1,63 @@
--- Hunter XX - Enraged Enhancer
-local s, id = GetID()
+--Hunter XX - Enraged Enhancer
+local s,id=GetID()
 function s.initial_effect(c)
-  -- effects
+  c:EnableReviveLimit()
+	--Special Summon Condition
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e0:SetValue(aux.FALSE)
+	c:RegisterEffect(e0)
+  --ATK change
+  local e1=Effect.CreateEffect(c)
+  e1:SetType(EFFECT_TYPE_SINGLE)
+  e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+  e1:SetCondition(s.condition1)
+  e1:SetValue(s.value1)
+  c:RegisterEffect(e1)
+  --Special Summon
+  local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TODECK + CATEGORY_SPECIAL_SUMMON)
+  e2:SetDescription(aux.Stringid(id,0))
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_PHASE+PHASE_END)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
+	e2:SetCountLimit(1)
+	c:RegisterEffect(e2)
+end
+function s.condition1(e)
+  local bc=e:GetHandler():GetBattleTarget()
+  return Duel.GetCurrentPhase()==PHASE_DAMAGE_CAL and bc and bc:IsControler(1-e:GetHandlerPlayer())
+end
+function s.value1(e)
+  local bc=e:GetHandler():GetBattleTarget()
+  if bc and bc:IsRelateToBattle() and bc:IsControler(1-e:GetHandlerPlayer()) then
+    return bc:GetAttack()*2
+  else
+    return 0
+  end
+end
+function s.filter2(c,e,tp)
+	return c:IsSetCard(0xf01) and not c:IsCode(20000354) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return true end
+  Duel.SetOperationInfo(0,CATEGORY_TODECK,e:GetHandler(),1,0,0)
+  Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+  local c=e:GetHandler()
+  if c:IsRelateToEffect(e) and c:IsFaceup() then
+    Duel.SendtoDeck(c,nil,2,REASON_EFFECT)
+    if c:IsLocation(LOCATION_EXTRA) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+      Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+      local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
+      if g:GetCount()>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+      end
+    end
+  end
 end
