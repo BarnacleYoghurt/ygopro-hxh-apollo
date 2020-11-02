76db44b893e75bda1a51294cdfef437fe2588047
utso <cccygodev@gmail.com>
2020-11-02 09:24:45 +0100

    Implement God Speed Transmutator

--- cards-hxh1/script/c20000350.lua
+++ cards-hxh1/script/c20000350.lua
@@ -1,5 +1,51 @@
--- Hunter XX - God Speed Transmutator
-local s, id = GetID()
+--Hunter XX - God Speed Transmutator
+local s,id=GetID()
 function s.initial_effect(c)
-  -- effects
+  c:EnableReviveLimit()
+	--Special Summon Condition
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e0:SetValue(aux.FALSE)
+	c:RegisterEffect(e0)
+  --Cannot activate
+  local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(0,1)
+	e1:SetCondition(s.condition1)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+  --Attack All
+  local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_ATTACK_ALL)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+  --Destroy Replace
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_DESTROY_REPLACE)
+	e3:SetTarget(s.target3)
+	c:RegisterEffect(e3)
+end
+function s.condition1(e)
+	local ph=Duel.GetCurrentPhase()
+	return ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE
+end
+function s.filter3(c)
+  return c:IsSetCard(0xf01) and c:IsType(TYPE_MONSTER) and c:IsDiscardable(REASON_EFFECT+REASON_REPLACE)
+end
+function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return not c:IsReason(REASON_REPLACE) and Duel.IsExistingMatchingCard(s.filter3,tp,LOCATION_HAND,0,1,nil) end
+	if Duel.SelectEffectYesNo(tp,c,96) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+		local g=Duel.SelectMatchingCard(tp,s.filter3,tp,LOCATION_HAND,0,1,1,nil)
+		Duel.SendtoGrave(g,REASON_DISCARD+REASON_EFFECT+REASON_REPLACE)
+		return true
+	else return false end
 end
