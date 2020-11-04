0f3b5a20fe6375277c702b6c86eb1981f7b42568
utso <cccygodev@gmail.com>
2020-11-04 18:56:49 +0100

    Raging Emisor: Finish implementation

--- cards-hxh1/script/c20000397.lua
+++ cards-hxh1/script/c20000397.lua
@@ -24,26 +24,57 @@ function s.initial_effect(c)
   e2:SetCondition(s.condition2)
   c:RegisterEffect(e2)
 	--Damage reduce
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
-	e4:SetCondition(s.condition4)
-	e4:SetValue(s.value4)
-	c:RegisterEffect(e4)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
+	e3:SetCondition(s.condition3)
+	e3:SetValue(s.value3)
+	c:RegisterEffect(e3)
+  --No target
+  local e4=Effect.CreateEffect(c)
+  e4:SetRange(LOCATION_MZONE)
+	e4:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+  e4:SetType(EFFECT_TYPE_FIELD)
+  e4:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+  e4:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+  e4:SetTarget(s.target4)
+  e4:SetValue(aux.tgoval)
+  c:RegisterEffect(e4)
+  --Indestructible
+  local e5=Effect.CreateEffect(c)
+  e5:SetRange(LOCATION_MZONE)
+  e5:SetType(EFFECT_TYPE_FIELD)
+  e5:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+  e5:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+  e5:SetTarget(s.target4)
+  e5:SetValue(aux.indoval)
+  c:RegisterEffect(e5)
+  --Indestructible once
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_FIELD)
+	e6:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetTargetRange(LOCATION_MZONE,0)
+	e6:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0xf01))
+	e6:SetValue(1)
+	c:RegisterEffect(e6)
 end
 function s.condition2(e)
   return Duel.GetAttacker()==e:GetHandler() and e:GetHandler():IsDefensePos()
 end
-function s.condition4(e)
+function s.condition3(e)
 	local c=e:GetHandler()
 	local tp=e:GetHandlerPlayer()
 	return Duel.GetAttackTarget()==nil and c:GetEffectCount(EFFECT_DIRECT_ATTACK)<2 
     and (Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0 or e:GetHandler():IsDefensePos()) --important to check both because another effect might allow direct defense attack on empty field
 end
-function s.value4(e,damp)
+function s.value3(e,damp)
 	if damp==1-e:GetHandlerPlayer() then
 		return e:GetHandler():GetBaseAttack()
 	else
 		return -1
 	end
+end
+function s.target4(e,c)
+  return c:IsFaceup() and c:IsSetCard(0xf01) and c~=e:GetHandler()
 end
