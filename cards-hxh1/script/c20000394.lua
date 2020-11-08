b444726c3291c44ed9b3ef3ab456a7fa8489e5c3
utso <cccygodev@gmail.com>
2020-11-08 14:19:38 +0100

    Enraged Enhancer: Adjust parameters of ATK gain effect based on Clear Vice Dragon

--- cards-hxh1/script/c20000394.lua
+++ cards-hxh1/script/c20000394.lua
@@ -11,6 +11,8 @@ function s.initial_effect(c)
 	c:RegisterEffect(e0)
   --ATK change
   local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
   e1:SetType(EFFECT_TYPE_SINGLE)
   e1:SetCode(EFFECT_SET_ATTACK_FINAL)
   e1:SetCondition(s.condition1)
