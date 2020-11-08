61491f87841240453b24304cd1fe658323b5a5b4
utso <cccygodev@gmail.com>
2020-11-08 14:32:26 +0100

    Departure: Final Check

--- cards-hxh1/script/c20000377.lua
+++ cards-hxh1/script/c20000377.lua
@@ -2,7 +2,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
   --Activate
-  local e1 = Effect.CreateEffect(c)
+  local e1=Effect.CreateEffect(c)
   e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
   e1:SetType(EFFECT_TYPE_ACTIVATE)
   e1:SetCode(EVENT_FREE_CHAIN)
