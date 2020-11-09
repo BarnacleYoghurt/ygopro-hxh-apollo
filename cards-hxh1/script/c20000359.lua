4b298170fe2e8eac6a32cd54cc90c2616204157b
utso <cccygodev@gmail.com>
2020-11-09 13:05:27 +0100

    Kaminari: Final check

--- cards-hxh1/script/c20000359.lua
+++ cards-hxh1/script/c20000359.lua
@@ -3,8 +3,8 @@ local s,id=GetID()
 function s.initial_effect(c)
   --Activate
   local e1 = Effect.CreateEffect(c)
-  e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
   e1:SetType(EFFECT_TYPE_ACTIVATE)
+  e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
   e1:SetCode(EVENT_FREE_CHAIN)
   e1:SetTarget(s.target1)
   e1:SetOperation(s.operation1)
@@ -15,7 +15,7 @@ function s.filter1(c)
   return c:IsFaceup() and c:IsSetCard(0xf01)
 end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-  if chkc then return chkc:IsCanBeEffectTarget(e) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter1(chkc) end
+  if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter1(chkc) end
   if chk==0 then return Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil) end
   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
   local g=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil)
